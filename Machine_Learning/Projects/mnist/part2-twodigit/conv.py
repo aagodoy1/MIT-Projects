3aa87b874075a45f1b7c01b365220956d220ada3
aagodoy1 <aagodoy1@uc.cl>
2025-03-21 15:04:46 -0400

    Finish project 3

--- Machine_Learning/Projects/mnist/part2-twodigit/conv.py
+++ Machine_Learning/Projects/mnist/part2-twodigit/conv.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from train_utils import batchify_data, run_epoch, train_model, Flatten
 import utils_multiMNIST as U
-path_to_data_dir = '../Datasets/'
+path_to_data_dir = 'C:/Users/user/Documents/GitHub/MIT-Projects/Machine_Learning/Projects/mnist/Datasets/'
+#path_to_data_dir = '../Datasets/'
 use_mini_dataset = True
 
 batch_size = 64
@@ -20,9 +21,47 @@ class CNN(nn.Module):
     def __init__(self, input_dimension):
         super(CNN, self).__init__()
         # TODO initialize model layers here
+        # Crear capas convulucionales
+        self.conv_layers = nn.Sequential(
+                                     # Cada capa de 3x3 reduce en 2 las dimensiones, ya que tenemos que padding = 0 y stride = 1 (default). 
+                                    # K: Tamaño kernel(filtro)
+                                    # S: Stride(default = 1) cuantos pixeles se mueve hacia el lado cada filtro
+                                    # P: Padding(default = 0) pixeles extra que se agrega para no perder informacion de la imagen. (relleno)
+                                    # Por lo tanto,
+                                    # El calculo es: 
+                                            # OUT = (IN - K + 2P)/S + 1 
+                                    # En ese caso seria (28-3+2*0)/1 + 1 = 26, por lo que cada filtro 3x3 reduce en 2 el tamaño
 
+                                    #Entra imagen de 1x42x28
+                                    nn.Conv2d(1, 32, (3, 3)), # Capa convulucional 2d: 1 entrada, 32 feature maps, los filtros tienen tamaño de 3x3 
+                                    # Se convierte en imagen de 32 x 40 x 26
+                                    nn.ReLU(), # Relu
+                                    nn.MaxPool2d((2, 2)), #Max pool de 2x2
+                                    # Se convierte en filtro de 32 x 20 x 13
+                                    nn.Conv2d(32, 64, (3, 3)), # Capa convulucional 2d: 1 entrada, 64 feature maps, los filtros tienen tamaño de 3x3
+                                    # sE convierte en 64 x 18 x 11
+                                    nn.ReLU(), # Relu
+                                    nn.MaxPool2d((2, 2))
+                                    # Se convierte en 64x9x5
+        )
+        self.flatten_and_hidden =  nn.Sequential(
+                                            Flatten(), #Se agrega una flatten layer
+                                            # Deja un vector de 64*5*5
+                                            nn.Linear(64*9*5, 128), 
+                                            nn.ReLU(),
+                                            nn.Dropout(0.5)
+        )
+        self.model1 = nn.Sequential(
+                                nn.Linear(128,10) # capa de salida
+        )
+        self.model2 = nn.Sequential(
+                                nn.Linear(128,10) # capa de salida
+        )
     def forward(self, x):
-
+        x = self.conv_layers(x)
+        x = self.flatten_and_hidden(x)
+        out_first_digit = self.model1(x)
+        out_second_digit = self.model2(x)
         # TODO use model layers to predict the two digits
 
         return out_first_digit, out_second_digit
--- Machine_Learning/Projects/mnist/part2-twodigit/mlp.py
+++ Machine_Learning/Projects/mnist/part2-twodigit/mlp.py
@@ -4,7 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from train_utils import batchify_data, run_epoch, train_model, Flatten
 import utils_multiMNIST as U
-path_to_data_dir = '../Datasets/'
+#sys.path.append(r"C:\Users\user\Documents\GitHub\MIT-Projects\Machine_Learning\Projects\mnist")
+#path_to_data_dir = '../Datasets/'
+path_to_data_dir = 'C:/Users/user/Documents/GitHub/MIT-Projects/Machine_Learning/Projects/mnist/Datasets/'
 use_mini_dataset = True
 
 batch_size = 64
@@ -12,21 +14,62 @@ nb_classes = 10
 nb_epoch = 30
 num_classes = 10
 img_rows, img_cols = 42, 28 # input image dimensions
-
+'''
 class MLP(nn.Module):
 
     def __init__(self, input_dimension):
         super(MLP, self).__init__()
         self.flatten = Flatten()
+        #Se crea una sola capa oculta que es compartida por ambos modelos para que el vector W de pesos se entrene tomando ambos en cuenta
+        # Esto lo hacemos solo porque los numeros vienen de una misma imagen, si vinieran de 2 diferentes, usariamos 2 capas distintas.
+        self.hidden_layer= nn.Sequential(
+                                    nn.Linear(input_dimension, 64), #hidden layer
+                                    nn.ReLU()
+        )
+        #Se crean las 2 capas de salida
+        self.model1 = nn.Sequential(nn.Linear(64, 10)) #output layer
+        self.model2 = nn.Sequential(nn.Linear(64, 10)) #output layer
         # TODO initialize model layers here
 
     def forward(self, x):
         xf = self.flatten(x)
-
         # TODO use model layers to predict the two digits
+        
+        hidden_out = self.hidden_layer(xf)  # Pasar por la capa oculta compartida
+        
+        # Cada salida usa la misma representación oculta
+        out_first_digit = self.model1(hidden_out)
+        out_second_digit = self.model2(hidden_out)
 
         return out_first_digit, out_second_digit
+'''
+class MLP(nn.Module):
 
+    def __init__(self, input_dimension):
+        super(MLP, self).__init__()
+        self.flatten = Flatten()
+        self.model1 = nn.Sequential(
+                                    nn.Linear(input_dimension, 64), #hidden layer
+                                    nn.ReLU(),
+                                    nn.Linear(64, 10), #output layer
+        )
+        self.model2 = nn.Sequential(
+                                    nn.Linear(input_dimension, 64), #hidden layer
+                                    nn.ReLU(),
+                                    nn.Linear(64, 10), #output layer
+        )
+        # TODO initialize model layers here
+
+    def forward(self, x):
+        xf = self.flatten(x)
+        # TODO use model layers to predict the two digits
+        
+        # Dos salidas para los dos dígitos
+        out_first_digit = self.model1(xf)
+        out_second_digit = self.model2(xf)
+
+        return out_first_digit, out_second_digit
+    
 def main():
     X_train, y_train, X_test, y_test = U.get_data(path_to_data_dir, use_mini_dataset)
 
